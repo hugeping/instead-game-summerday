abce7526ef39b1a65f44969ca103fa6a59bd070b
Peter Kosyh <p.kosyh@gmail.com>
2019-03-21 15:05:18 +0300

    up

--- main3.lua
+++ main3.lua
@@ -15,6 +15,24 @@ Verb {
 	"Wake";
 }
 
+-- уйти к дому
+VerbExtend {
+	"#Exit";
+	"~ к {noun}/дт,scene: Walk";
+}
+-- чтоб можно было писать "на кухню" вместо "идти на кухню"
+
+function mp:pre_input(str)
+	local a = std.split(str)
+	if #a <= 1 or #a > 3 then
+		return str
+	end
+	if a[1] == 'в' or a[1] == 'на' or a[1] == 'во' then
+		return "идти "..str
+	end
+	return str
+end
+
 Path = Class {
 	['before_Walk,Enter'] = function(s) walk (s.walk_to) end;
 	before_Default = function(s)
@@ -342,11 +360,17 @@ Verb {
 -- спички нужны
 -- враги: паук(огонь), крыса(лук)
 
-declare 'make_p' (function()
-		return obj {
-			-"пирожок";
-		}:attr'edible'
-		 end)
+global 'pie_nr' (0)
+
+obj {
+	nam = 'pie';
+	-"пирожок";
+	after_Eat = function(s)
+		pie_nr = pie_nr - 1
+		p "Ты с удовольствием умял бабушкин пирожок."
+	end;
+}:attr'edible'
+
 obj {
 	-"бабушка";
 	found_in = 'kitchenroom';
@@ -354,8 +378,13 @@ obj {
 	before_Kiss = [[-- Осторожно, внучик, мука!]];
 	dsc = [[Ты видишь как бабушка делает пирожки.]];
 	['before_Talk,Say,Ask,Tell'] = function(s)
-		p [[-- Держи пирожок!]];
-		take(new (make_p))
+		if pie_nr == 0 then
+			p [[-- Держи пирожок!]];
+			pie_nr = pie_nr + 1
+			take 'pie'
+		else
+			p [[-- Сначала съешь тот, что я тебе уже дала -- улыбается бабушка.]];
+		end
 	end;
 }:attr'animated'
 
@@ -436,11 +465,13 @@ obj {
 }:attr 'static,enterable,openable,lockable,locked';
 
 obj {
-	-"цветы|клумбы";
+	-"цветы/мр|клумбы";
 	found_in = 'street';
 	description = [[Ты не разбираешься в цветах, но они очень красивые и хорошо пахнут.]];
 	before_Take = [[Зачем зря рвать цветы?]];
-}:attr'scenery';
+}:attr'scenery':dict{
+	["цветы/вн"] = "цветы";
+}
 
 room {
 	-"улица";
@@ -448,7 +479,7 @@ room {
 	title = "На улице";
 	Smell = [[Запах цветов кружит тебе голову.]];
 	dsc = [[Ты стоишь на улице возле своего дома, утопающего в цветах. Отсюда ты можешь пойти на футбольное поле
-или во двор.]];
+или во двор к пятиэтажке.]];
 	in_to = 'house';
 }: with {
 	obj {
@@ -463,6 +494,11 @@ room {
 		-"футбольное поле|поле";
 		walk_to = 'field';
 		desc = [[Ты можешь пойти на футбольное поле.]];
+	};
+	Path {
+		-"пятиэтажка|двор";
+		walk_to = 'houses';
+		desc = [[Ты можешь пойти к пятиэтажке.]];
 	};
 }
 
@@ -499,7 +535,7 @@ Verb {
 obj {
 	-"удочка";
 	nam = 'bow';
-	description = [[Старая удочка из гибкого бамбука. Здорово гнётся!]];
+	description = [[Старая удочка из гибкого и крепкого бамбука. Здорово гнётся!]];
 	found_in = 'junk';
 }:disable();
 
@@ -549,17 +585,176 @@ room {
 	Path {
 		-"улица";
 		walk_to = 'street';
-		descr = [[Ты можешь пойти на улицу.]];
+		desc = [[Ты можешь пойти на улицу.]];
 	};
 }
 
 room {
 	-"поле";
+	nam = 'field';
 	title = "футбольное поле";
+	Smell = [[Пахнет душистым клевером.]];
+	dsc = [[]];
 }: with {
 	Path {
 		-"дом";
 		walk_to = 'street';
-		descr = [[Ты можешь пойти к своему дому.]];
+		desc = [[Ты можешь пойти к своему дому.]];
+	}
+}
+
+obj {
+	-"пятиэтажка,кирпич*|пятиэтажный дом";
+	found_in = 'houses';
+	description = [[Пятиэтажка построена из красного кирпича.]];
+}:attr'scenery';
+
+obj {
+	-"ребята";
+	found_in = 'houses';
+	before_Default = "Здесь есть Света, Максим и Руслан.";
+	dsc = [[Ребята стоят у входа в подвал.]];
+};
+
+obj {
+	-"Света|девочка";
+	nam = 'girl';
+	found_in = 'houses';
+	talk_to = function(s)
+		if _'underground':hasnt'locked' then
+			p [[-- Какой ты молодец! Ты спасёшь Мурзика? Правда?]]
+			return
+		end
+		p [[-- Бедный Мурзик! Говорят, в подвале много-много крыс! Что же делать? Может быть, ты что-нибудь придумаешь?]];
+	end;
+	description = [[Тебе кажется, что Света очень красивая.]];
+	['before_Kiss,Touch,Taste,Smell,Take'] = "У тебя не хватает духу.";
+}:attr'animated'
+
+obj {
+	-"Руслан";
+	nam = 'serge';
+	found_in = 'houses';
+	talk_to = function(s)
+		if _'underground':hasnt'locked' then
+			p [[-- Ух ты! Обычной скрепкой! Но в подвале темно. Кстати, Ромке отец подарил охотничьи спички. Я его видел недавно, он шел на футбольное поле.]]
+			return
+		end
+		p [[-- Он залез в окно, а вылезти не может. Только всё время пищит.]];
+	end;
+	description = [[Руслан довольно пухлый мальчик в очках. Он увлекается шахматами.]];
+	['before_Kiss,Touch,Taste,Smell'] = "Ну уж нет.";
+}:attr'animated'
+
+obj {
+	-"Макс,Максим";
+	nam = 'max';
+	found_in = 'houses';
+	talk_to = function(s)
+		if _'underground':hasnt'locked' then
+			p [[-- В подвале темно и страшно! Я бы не рискнул спуститься туда.]]
+			return
+		end
+		p [[-- Мурзик залез в подвал и не может выбраться! Наверное, его съедят крысы!]];
+	end;
+	description = [[Макс -- маленький юркий паренёк. Он младше тебя на год.]];
+	['before_Kiss,Touch,Taste,Smell'] = "У тебя нет такого желания.";
+}:attr'animated'
+
+
+
+obj {
+	-"подвал";
+	nam = 'underground';
+	with_key = 'wire';
+	found_in = 'houses';
+	description = function(s)
+		if s:has'locked' then
+			p [[Подвал закрыт на замок.]]
+		else
+			return false
+		end
+	end;
+	before_Attack = function(s)
+		mp:xaction("Attack", _'#guard')
+	end;
+	before_Lock = function(s)
+		if s:hasnt'locked' then
+			p [[Второй раз может не получиться.]]
+		else
+			return false
+		end
+	end;
+	after_Unlock = function(s)
+		pn [[Не надеясь на успех, ты всё-таки попробовал открыть замок скрепкой...]]
+		p [[У тебя это {$fmt em|получилось}!]];
+		s:attr'open'
+	end;
+}:attr 'scenery,openable,locked,lockable': with{
+	obj {
+		-"замок";
+		nam = '#guard';
+		description = [[Маленький стальной замок.]];
+		before_Take = [[Это государственное имущество.]];
+		before_Attack = [[Замок железный, так просто его не сломать.]];
+		before_Open = function(s)
+			if _'underground':has'locked' then
+				p [[Но как?]]
+			else
+				p [[Уже открыт.]];
+			end
+		end;
+		['before_Lock,Close'] = function(s, w)
+			mp:xaction('Lock', _'underground', w)
+		end;
+		before_Unlock = function(s, w)
+			mp:xaction('Unlock', _'underground', w)
+		end;
+		before_Receive = function(s, wh)
+			if wh ^ 'wire' then
+				mp:xaction("Unlock", _'underground', _'wire')
+				return
+			end
+			return false
+		end;
+	}:attr'scenery';
+
+}
+
+room {
+	nam = 'houses';
+	-"двор";
+	title = 'Во дворе пятиэтажки';
+	in_to = 'underground';
+	out_to = 'street';
+	before_Listen = function(s, wh)
+		if _'underground':hasnt'locked' or wh == _'underground' then
+			p [[Из глубины подвала ты слышишь жалобный писк котёнка.]];
+			return
+		end
+		local txt = {
+			[[-- Ему нужно как-то помочь!]];
+			[[-- Там же темно!]];
+			[[-- Я слышал, в подвале водятся крысы...]];
+			[[-- Что, так и будем стоять?]];
+			[[-- Бедный Мурзик!]];
+		};
+		p ("Ты слышишь оживлённый детский спор. Сложно разобрать, чем он вызван.")
+		p "^"
+		p (txt[rnd(#txt)], " -- говорит кто-то из ребят.")
+	end;
+	dsc = function(s)
+		p [[У кирпичной пятиэтажки есть большой двор, где обычно играют дети из соседних домов.]];
+		if s:once() then
+			pn "^"
+			pn [[Ты заметил Свету, Руслана и Макса. Они о чём-то спорят.]];
+		end
+		p [[Ты можешь уйти к своему дому.]];
+	end;
+}: with {
+	Path {
+		-"дом";
+		walk_to = 'street';
+		desc = [[Ты можешь пойти к своему дому.]];
 	}
 }
